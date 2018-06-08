4f00d3c2e64491dacb1dbb04cdae4eaed0e5b3ad
Wanderson Passos Barcelos <wp.barcelos@gmail.com>
2018-06-08 01:41:05 -0300

    adicionado frontend com create-react-app + bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
