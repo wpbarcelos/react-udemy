801e775ae159576e0b4b43604356770208437cc0
Desenvolvimento Container Solution <desenvolvimento@containersol.com.br>
2018-08-14 18:36:40 -0300

    aula 77

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,9 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reducers from './main/reducers';
 
-const store = createStore(reducers);
+const devTools =  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducers, devTools);
 
 // import registerServiceWorker from './registerServiceWorker';
 
--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -1,4 +1,4 @@
 export const changeDescription = event => ({
-    type: 'DESCRITION_CHANGED',
+    type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
